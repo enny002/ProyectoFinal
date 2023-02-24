5116b23a7d3ce298dfb907903841116a2b6a0e48
enny002 <bmopua@hotmail.com>
2023-02-23 23:11:58 -0500

    version 0.1

--- Catalogo/dist/formulario.js
+++ Catalogo/dist/formulario.js
@@ -1,8 +1,8 @@
-import {Validacion} from "./Validacion.js";/* */
+import {Validacion} from "./Validacion.js";
 
 /*Instanciar el objeto*/
 
-const validacion = new Validacion(); /**/
+const validacion = new Validacion(); 
 const formulario = document.getElementById('form');
 const btn = document.getElementById("btnSend");
 
@@ -15,24 +15,24 @@ const formValid ={
 }
 
 //Validación
-formulario.addEventListener("change", (e)=>{/**/
+formulario.addEventListener("change", (e)=>{
     const inputId = e.target.id;
-    const miValue = e.target.value;/**/
+    const miValue = e.target.value;
     const miClass = e.target.classList;
     const validClass = ()=>{
         miClass.add("is-valid");
-        miClass.remove("is-invalid");/**/
+        miClass.remove("is-invalid");
     };
     const inValidClass = ()=>{
         miClass.add("is-invalid");
         miClass.remove("is-valid");
     };
 
-    switch(inputId){/**/
-        case "names":/**/
-            formValid.nombres = validacion.validNames(miValue);/**/
-            formValid.nombres ? validClass() : inValidClass();/**/
-            console.log(Object.values(formValid));/**/
+    switch(inputId){/*evalúa una expresión, comparando el valor de esa expresión con una instancia case , y ejecuta declaraciones asociadas a ese case , así como las declaraciones en los case que siguen.*/
+        case "names":/*es una instancia del switch que será evaluada*/
+            formValid.nombres = validacion.validNames(miValue);
+            formValid.nombres ? validClass() : inValidClass();
+            console.log(Object.values(formValid));
             break;
         case "mail":
             formValid.mail = validacion.validMail(miValue);
@@ -48,11 +48,11 @@ formulario.addEventListener("change", (e)=>{/**/
 });
 
 //Envío del formulario
-btn.addEventListener("click", (e)=>{/**/
-    e.preventDefault();/**/
+btn.addEventListener("click", (e)=>{/*Registra un evento a un objeto en específico. El Objeto especifico (en-US) puede ser un simple elemento en un archivo, el mismo documento , una ventana o un XMLHttpRequest.*/
+    e.preventDefault();/*Cancela el evento si este es cancelable, sin detener el resto del funcionamiento del evento, es decir, puede ser llamado de nuevo. */
     if (validacion.validarForm(formValid)=== -1){/**/
-        alert("Enviando Formulario");/**/
-        let datos = new FormData(document.getElementById('form'));/**/
+        alert("Enviando Formulario");/*muestra un diálogo de alerta con un mensaje opcional, y aguardará hasta que el usuario cierre la ventana de diálogo.*/
+        let datos = new FormData(document.getElementById('form'));/*forData: proporciona una manera sencilla de construir un conjunto de parejas clave/valor que representan los campos de un formulario y sus valores, que pueden ser enviados fácilmente con el método XMLHttpRequest.send() (en-US).*/
         console.log(datos);
     }
 });
--- Catalogo/dist/main.js
+++ Catalogo/dist/main.js
@@ -1,99 +1,102 @@
-const btnCart = document.querySelector('.container-cart-icon');/**/
-const containerCartProducts = document.querySelector(/**/
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector(
 	'.container-cart-products'
 );
 
-btnCart.addEventListener('click', () => {/**/
-	containerCartProducts.classList.toggle('hidden-cart');/**/
+btnCart.addEventListener('click', () => {
+	containerCartProducts.classList.toggle('hidden-cart');
 });
 
 /* ========================= */
-const cartInfo = document.querySelector('.cart-product');/**/
-const rowProduct = document.querySelector('.row-product');/**/
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
 
 // Lista de todos los contenedores de productos
-const productsList = document.querySelector('.container-items');/**/
+const productsList = document.querySelector('.container-items');
 
 // Variable de arreglos de Productos
-let allProducts = [];/**/
+let allProducts = [];
 
-const valorTotal = document.querySelector('.total-pagar');/**/
+const valorTotal = document.querySelector('.total-pagar');
 
-const countProducts = document.querySelector('#contador-productos');/**/
+const countProducts = document.querySelector('#contador-productos');
 
-const cartEmpty = document.querySelector('.cart-empty');/**/
-const cartTotal = document.querySelector('.cart-total');/**/
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
 
-productsList.addEventListener('click', e => {/**/
-	if (e.target.classList.contains('btn-add-cart')) {/**/
-		const product = e.target.parentElement;/**/
+productsList.addEventListener('click', e => {
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
 
-		const infoProduct = {/**/
-			quantity: 1,/**/
-			title: product.querySelector('h2').textContent,/**/
-			price: product.querySelector('p').textContent,/**/
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h2').textContent,
+			price: product.querySelector('p').textContent,
 		};
 
-		const exits = allProducts.some(/**/
-			product => product.title === infoProduct.title/**/
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
 		);
 
-		if (exits) {/**/
-			const products = allProducts.map(product => {/**/
-				if (product.title === infoProduct.title) {/**/
-					product.quantity++;/**/
-					return product;/**/
-				} else {/**/
+		if (exits) {
+			const products = allProducts.map(product => {
+				if (product.title === infoProduct.title) {
+					product.quantity++;
+					return product;
+				} else {
 					return product;
 				}
 			});
-			allProducts = [...products];/**/
+			allProducts = [...products];
 		} else {
 			allProducts = [...allProducts, infoProduct];
 		}
 
-		showHTML();/**/
+		showHTML();
 	}
 });
 
-rowProduct.addEventListener('click', e => {/**/
-	if (e.target.classList.contains('icon-close')) {/**/
-		const product = e.target.parentElement;/**/
+rowProduct.addEventListener('click', e => {
+	if (e.target.classList.contains('icon-close')) {
+		const product = e.target.parentElement;
 		const title = product.querySelector('p').textContent;
 
-		allProducts = allProducts.filter(/**/
-			product => product.title !== title/**/
+		allProducts = allProducts.filter(/*filter: s un atributo Booleano que indica que el elemento todavía no está , o ya no es relevante .*/
+			product => product.title !== title
 		);
 
-		console.log(allProducts);/**/
+		console.log(allProducts);
 
-		showHTML();/**/
+		showHTML();
 	}
 });
 
 // Funcion para mostrar  HTML
-const showHTML = () => {/**/
-	if (!allProducts.length) {/**/
-		cartEmpty.classList.remove('hidden');/**/
-		rowProduct.classList.add('hidden');/**/
-		cartTotal.classList.add('hidden');/**/
-	} else {/**/
-		cartEmpty.classList.add('hidden');/**/
-		rowProduct.classList.remove('hidden');/**/
-		cartTotal.classList.remove('hidden');/**/
+const showHTML = () => {/*funcion tipo flecha*/
+	if (!allProducts.length) {/*length: se utiliza para manipular series de caracteres de todos los tipos de datos de serie de caracteres (BIT, BLOB y CHARACTER) y devuelve un valor de entero que proporciona el número de elemento de source_string.*/
+		cartEmpty.classList.remove('hidden');/*empty: determina si una variable está vacía.*/
+		rowProduct.classList.add('hidden');/*hidden:  indica que el elemento todavía no está , o ya no es relevante .*/
+		cartTotal.classList.add('hidden');
+	} else {
+		cartEmpty.classList.add('hidden');
+		rowProduct.classList.remove('hidden');
+		cartTotal.classList.remove('hidden');
 	}
 
 	// Limpiar HTML
-	rowProduct.innerHTML = '';/**/
+	rowProduct.innerHTML = '';
 
 	let total = 0;
-	let totalOfProducts = 0;/* */
+	let totalOfProducts = 0;
 
-	allProducts.forEach(product => {/**/
-		const containerProduct = document.createElement('div');/**/
-		containerProduct.classList.add('cart-product');/**/
+	allProducts.forEach(product => {/*forEach: ejecuta la función callback una vez por cada elemento del array; a diferencia de map() o reduce() este siempre devuelve el valor undefined y no es encadenable. El típico uso es ejecutar los efectos secundarios al final de la cadena.*/
+		const containerProduct = document.createElement('div');
+		containerProduct.classList.add('cart-product');
 
-		/**/ containerProduct.innerHTML = `
+/*es una de estas propiedades comunes que podemos utilizar para modificar cualquier instancia de Element. La propiedad innerHTML, 
+también conocida como element.innerHTML o nodo.innerHTML, 
+recibe el código HTML que el nodo tiene que renderizar. Es decir, es otra manera de crear y ubicar elementos dentro del DOM*/
+		containerProduct.innerHTML = `
             <div class="info-cart-product">
                 <span class="cantidad-producto-carrito">${product.quantity}</span>
                 <p class="titulo-producto-carrito">${product.title}</p>
@@ -108,16 +111,16 @@ const showHTML = () => {/**/
                 <path stroke-linecap="round"
                     stroke-linejoin="round"
                     d="M6 18L18 6M6 6l12 12"/>
-            </svg>`;/**/
+            </svg>`;/*svg: es un contenedor que define un nuevo sistema de coordenadas y ventana gráfica .*/
 
-		rowProduct.append(containerProduct);/* */
+		rowProduct.append(containerProduct);/*método inserta un conjunto de Nodeobjetos u objetos de cadena después del último hijo del Element.  */
 
 		total =
-			total + parseInt(product.quantity * product.price.slice(1));/* */
-		totalOfProducts = totalOfProducts + product.quantity;/* */
+			total + parseInt(product.quantity * product.price.slice(1));/*analiza un argumento de cadena y devuelve un número entero de la raíz especificada (la base en los sistemas numéricos matemáticos). */
+		totalOfProducts = totalOfProducts + product.quantity;
 	});
 
-	valorTotal.innerText = `$${total}`;/* */
-	countProducts.innerText = totalOfProducts;/* */
+	valorTotal.innerText = `$${total}`;
+	countProducts.innerText = totalOfProducts;/*La innerTextpropiedad de la HTMLElementinterfaz representa el contenido de texto representado de un nodo y sus descendientes. */
 };
   
